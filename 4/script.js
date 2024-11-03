53af62c597eb989a93061a52ce5a55c153570c37
Priyanka Sarkar <prisar13@gmail.com>
2024-11-03 10:54:39 +0530

    Day 5

--- 4/script.js
+++ 4/script.js
@@ -2,7 +2,7 @@ const search=document.querySelector('.search');
 const btn= document.querySelector('.btn');
 const input=document.querySelector('.input');
 
-btn.addEventListener('click',()=>{
+btn.addEventListener('click', ()=>{
     search.classList.toggle('active');
     input.focus();
 })
